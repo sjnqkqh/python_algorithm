138ffdf8bebb4b6180051280c38d0c3836d5548f
양준렬 <sjnqkqh@naver.com>
2022-06-10 04:39:49 +0000

    ..

---
+++ 06.10/2869.py
@@ -0,0 +1,13 @@
+a,b,c = map(int, input().split())
+
+count, height  = 0;
+
+while(height < c)
+    height = height + a
+    count = count +1
+    
+    if (height >= c){
+        print(coount)
+        break
+    
+}
