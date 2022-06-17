39b09934d850d0967472e0a0144436535d3702c6
양준렬 <jlyang@deleo.co.kr>
2022-06-17 18:20:51 +0900

    3일전에 틀린문제 컫!!!컫!!컫!!!!@

---
+++ 2022.06/06.17/1748_S4.py
@@ -0,0 +1,18 @@
+# URL : https://www.acmicpc.net/problem/1748
+import math
+
+n = int(input())
+result = 0
+
+log10 = int(math.log10(n))
+
+for i in range(log10, -1, -1):
+    # n이 10보다 클 때
+    if i >= 1:
+        minus = n - (10 ** i) + 1
+        result = result + ((minus) * (i + 1))
+        n = n - minus
+    else:
+        result = result + n
+
+print(result)
