7a7360c806663d2ea6b8191e48d5e728668680f9
양준렬 <jlyang@deleo.co.kr>
2022-07-12 11:49:49 +0900

    힘두러잇...

--- 2022.07/07.11/2206_G4.py
+++ 2022.07/07.11/2206_G4.py
@@ -3,8 +3,7 @@ from collections import deque
 
 N, M = map(int, input().split())
 arr = []
-visited = [[[False, False] for _ in range(M)] for _ in range(N)]
-graph = [[[int(1e9), int(1e9)] for _ in range(M)] for _ in range(N)]
+visited = [[[0, 0] for _ in range(M)] for _ in range(N)]
 
 # 상하좌우 이동 배열
 move_arr = [[-1, 0], [1, 0], [0, -1], [0, 1]]
@@ -17,43 +16,29 @@ for _ in range(N):
 # # BFS
 def bfs():
     queue = deque()
-    queue.append([0, 0, True])
-    visited[0][0][0] = True  # 벽을 한 번도 뚫지 않은 상태에서 시작점 방문 처리
-    graph[0][0][0] = 1
-    graph[0][0][1] = 1
+    queue.append([0, 0, 0])
+    visited[0][0][0] = 1  # 벽을 한 번도 뚫지 않은 상태에서 시작점 방문 처리
 
     while queue:
         size = len(queue)
         for _ in range(size):
-            i, j, not_crashed_yet = queue.popleft()
+            i, j, wall = queue.popleft()
+
+            if i == N - 1 and j == M - 1:
+                return visited[i][j][wall]
 
             for move in move_arr:
                 next_i = i + move[0]
                 next_j = j + move[1]
-
                 if 0 <= next_i < N and 0 <= next_j < M:
-                    if arr[next_i][next_j] == 0:
-                        if not visited[next_i][next_j][0]:
-                            graph[next_i][next_j][0] = graph[i][j][0] + 1
-                            visited[next_i][next_j][0] = True
-                            queue.append([next_i, next_j, not_crashed_yet])
-                    #
-                    #     if not visited[next_i][next_j][1]:
-                    #         graph[next_i][next_j][1] = graph[i][j][0] + 1
-                    #         visited[next_i][next_j][1] = True
-                    #         queue.append([next_i, next_j, True])
-                    #
-                    # else:
-                    #     if not visited[next_i][next_j][1] and not_crashed_yet:
-                    #         graph[next_i][next_j][1] = graph[i][j][1] + 1
-                    #         visited[next_i][next_j][1] = True
-                    #         queue.append([next_i, next_j, False])
+                    if arr[next_i][next_j] == 0 and visited[next_i][next_j][wall] == 0:
+                        visited[next_i][next_j][wall] = visited[i][j][wall] + 1
+                        queue.append([next_i, next_j, wall])
 
-    result = min(graph[N - 1][M - 1][0], graph[N - 1][M - 1][1]) if visited[N - 1][M - 1][0] or visited[N - 1][M - 1][1] else -1
-    for item in graph:
-        print(item)
-
-    print(result)
+                    if arr[next_i][next_j] == 1 and wall == 0:  # 벽을 한 번도 부수지 않은 상태로, 벽을 마주했다면
+                        visited[next_i][next_j][1] = visited[i][j][wall] + 1
+                        queue.append([next_i, next_j, 1])
 
 
-bfs()
+result = bfs()
+print(result if result is not None else -1)
