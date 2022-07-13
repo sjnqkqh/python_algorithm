10cc5d5028390be766ee17bd76f8c9a1e86f6d23
양준렬 <jlyang@deleo.co.kr>
2022-07-13 14:18:46 +0900

    고르면서 기록하는걸로 성공

--- 2022.07/07.12/13913_G4.py
+++ 2022.07/07.12/13913_G4.py
@@ -1,30 +1,30 @@
-import sys
 from collections import deque
-from pprint import pprint
 
-sys.setrecursionlimit(int(1e9))
-
-N, K = map(int, input().split())
-INF = int(1e9)
-Length = 100001
+N, K = map(int, input().split()) # 데이터 입력
+Length = 100001 # 배열 총 길이
 arr = [-1 for _ in range(Length)]
 
-arr[N] = 0
+arr[N] = 0 # 탐색 시작 위치
+log = {}  # 이동 경로 Dictionary
+result_step = []  # 총 이동 경로 List
 
 
-# BFS
-def bfs(start):
+# BFS 탐색
+def bfs(search_start):
     queue = deque()
-    queue.append((start, 1))
+    queue.append((search_start, 1))
 
     while queue:
+        # Queue의 사이즈 만큼만 반복
         size = len(queue)
         for _ in range(size):
             now, cnt = queue.popleft()
 
+            # 탐색 중 목표지 도달 시, 최소 이동 횟수 반환
             if now == K:
                 return cnt
 
+            # Node 이동 가능 범위에 따라 다음 탐색 인덱스 추가 및, Dict 에 경로 저장
             if 0 <= now + 1 < Length and arr[now + 1] == -1:
                 arr[now + 1] = cnt
                 queue.append((now + 1, cnt + 1))
@@ -41,76 +41,20 @@ def bfs(start):
                 log[now * 2] = now
 
 
-# def tracking_dfs(start, depth, visited, log: list):
-#     visited[start] = True
-#     log.append(start)
-#
-#     # 트랙킹 종료 시, 리스트 반전하여 출력
-#     if depth == 1:
-#         log.reverse()
-#         print(*log, sep=" ")
-#         return
-#
-#     if 0 <= start - 1 < Length and depth - 2 == arr[start - 1] and not visited[start-1]:
-#         tracking_dfs(start - 1, depth - 1, visited, log)
-#         return
-#
-#     if start % 2 == 0 and depth - 2 == arr[start // 2] and not visited[start-1]:
-#         tracking_dfs(start // 2, depth - 1, visited, log)
-#         return
-#
-#     if 0 <= start + 1 < Length and depth - 2 == arr[start + 1] and not visited[start-1]:
-#         tracking_dfs(start + 1, depth - 1, visited, log)
-#         return
-#
-# def tracking_bfs():
-#     visited = [False for _ in range(Length)]
-#     queue = deque()
-#     queue.append(K)
-#     step = depth
-#     log = [K]
-#
-#     while queue:
-#         size = len(queue)
-#
-#         for _ in range(size):
-#             point = queue.popleft()
-#
-#             if step == 0:
-#                 print(*log, sep=" ")
-#                 return
-#
-#             if 0 <= point // 2 < Length and point % 2 == 0 and arr[point // 2] == step-1 and not visited[point // 2]:
-#                 visited[point // 2] = True
-#                 queue.append(point // 2)
-#                 log.append(point//2)
-#                 break
-#
-#             if 0 <= point - 1 < Length and arr[point - 1] == step-1 and not visited[point - 1]:
-#                 visited[point - 1] = True
-#                 queue.append(point - 1)
-#                 log.append(point -1)
-#                 break
-#
-#             if 0 <= point + 1 < Length and arr[point + 1] == step-1 and not visited[point + 1]:
-#                 visited[point + 1] = True
-#                 queue.append(point + 1)
-#                 log.append(point+1)
-#                 break
-#
-#         step -= 1
+# BFS 탐색
+result = bfs(N) - 1
 
-log = {}
-depth = bfs(N) - 1
-print(depth)  # 최소 이동횟수
-
+# K부터 N까지 경로 Dictionary 를 탐색해, 역으로 경로 탐색
 start = K
-resultStep = []
 while True:
-    resultStep.append(start)
+    result_step.append(start)
     if start == N:
         break
     start = log[start]
 
-resultStep.reverse()
-print(*resultStep, sep=" ")
+# 배열 반전
+result_step.reverse()
+
+# 답안 출력
+print(result)  # 최소 이동횟수
+print(*result_step, sep=" ")  # 최소 이동 경로
