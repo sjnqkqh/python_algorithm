5bf612c5a58e1f5428d1724e7e8a414f409f1e44
양준렬 <jlyang@deleo.co.kr>
2022-08-05 17:30:17 +0900

    발전했다. 그게 위로가 된다.

---
+++ 2022.08/08.05/팰린드롬_G4.py
@@ -0,0 +1,28 @@
+N = int(input())
+arr = list(map(int, input().split()))
+num_dict = dict()
+result = 0
+
+if N % 2 == 0:
+    pre_list = arr[:N // 2]
+    post_list = arr[N // 2:]
+else:
+    pre_list = arr[:N // 2]
+    post_list = arr[(N // 2) + 1:]
+
+post_list.reverse()
+print(pre_list)
+print(post_list)
+LCS = [[0] * (len(pre_list) + 1) for _ in range(len(pre_list) + 1)]
+
+print(LCS)
+
+for i in range(1, len(post_list)):
+    for j in range(i, len(post_list)):
+        print(i, j)
+        if pre_list[i] == post_list[i]:
+            LCS[i][j] = LCS[i - 1][j - 1] + 1
+        else:
+            LCS[i][j] = max(LCS[i - 1][j], LCS[i][j - 1])
+
+print(LCS)
