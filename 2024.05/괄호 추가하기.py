880ce98828a6dea090e506f2ce84f772e8f60694
real-dev <sjn3649@naver.com>
2024-05-17 23:01:37 +0900

    괄호 추가하기 Solve - 이중 괄호 케이스는 문제가 아니었고, 음수로 계산하다 음수를 다시 곱해서 뻥튀기되는 케이스가 존재하는 거였음. 인생

--- 2024.05/괄호 추가하기.py
+++ 2024.05/괄호 추가하기.py
@@ -1,6 +1,10 @@
 n = int(input())
 int_str = input()
-dp = [-int(1E9)] * n
+MIN = -int(2 ** 31)
+MAX = int(2 ** 31)
+max_dp = [MIN] * n
+min_dp = [MAX] * n
+
 arr = []
 
 for i in range(n):
@@ -19,13 +23,27 @@ def calc(now, target, v):
         return target * now
 
 
-dp[0] = arr[0]
+max_dp[0] = arr[0]
+min_dp[0] = arr[0]
 if n >= 3:
-    dp[1] = dp[0]
-    dp[2] = calc(arr[2], arr[0], arr[1])
+    max_dp[1] = max_dp[0]
+    max_dp[2] = calc(arr[2], arr[0], arr[1])
+    min_dp[1] = max_dp[0]
+    min_dp[2] = calc(arr[2], arr[0], arr[1])
 
 for i in range(4, n, 2):
-    dp[i] = max(calc(arr[i], dp[i - 2], arr[i - 1]),
-                calc(calc(arr[i], arr[i - 2], arr[i - 1]), dp[i - 4], arr[i - 3]))
+    max_dp[i] = max(
+        calc(arr[i], max_dp[i - 2], arr[i - 1]),
+        calc(arr[i], min_dp[i - 2], arr[i - 1]),
+        calc(calc(arr[i], arr[i - 2], arr[i - 1]), max_dp[i - 4], arr[i - 3]),
+        calc(calc(arr[i], arr[i - 2], arr[i - 1]), min_dp[i - 4], arr[i - 3]),
+    )
 
-print(dp[-1])
+    min_dp[i] = min(
+        calc(arr[i], max_dp[i - 2], arr[i - 1]),
+        calc(arr[i], min_dp[i - 2], arr[i - 1]),
+        calc(calc(arr[i], arr[i - 2], arr[i - 1]), max_dp[i - 4], arr[i - 3]),
+        calc(calc(arr[i], arr[i - 2], arr[i - 1]), min_dp[i - 4], arr[i - 3]),
+    )
+
+print(max(max_dp[-1], min_dp[-1]))
