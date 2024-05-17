fb2fe568f7fe444cb8d2d6ca5eba84aacacd82f1
real-dev <sjn3649@naver.com>
2024-05-17 17:15:44 +0900

    괄호 추가하기 2차 구현 완료. 단, 결과적으로 틀렸으며 이중 괄호 케이스를 계산하지 못한 것이 이유로 보임

--- 2024.05/괄호 추가하기.py
+++ 2024.05/괄호 추가하기.py
@@ -1,13 +1,13 @@
 n = int(input())
-str = input()
+int_str = input()
 dp = [-int(1E9)] * n
 arr = []
 
 for i in range(n):
     if i % 2 == 0:
-        arr.append(int(str[i]))
+        arr.append(int(int_str[i]))
     else:
-        arr.append(str[i])
+        arr.append(int_str[i])
 
 
 def calc(now, target, v):
@@ -20,12 +20,12 @@ def calc(now, target, v):
 
 
 dp[0] = arr[0]
-if n > 1:
+if n >= 3:
     dp[1] = dp[0]
     dp[2] = calc(arr[2], arr[0], arr[1])
 
 for i in range(4, n, 2):
-    dp[i] = max(calc(arr[i], dp[i - 2], arr[i-1]),
-                calc(dp[i - 4], calc(arr[i], arr[i - 2], arr[i-1]), arr[i-3]))
+    dp[i] = max(calc(arr[i], dp[i - 2], arr[i - 1]),
+                calc(calc(arr[i], arr[i - 2], arr[i - 1]), dp[i - 4], arr[i - 3]))
 
-    print(dp)
+print(dp[-1])
